4f2432a0d99a2c276ee46a6a2359d80f2e85c604
James Attard <james.a.attard@mcast.edu.mt>
2025-11-17 13:29:20 +0000

    Created basic named routes

--- contactApp/resources/views/welcome.blade.php
+++ contactApp/resources/views/welcome.blade.php
@@ -1,1 +1,9 @@
-<title>SSS Laravel</title>
+<html>
+    <title>SSS Laravel</title>
+
+    <div>
+        <li><a href="{{ route('contacts.index') }}">All Contacts</a></li>
+        <li><a href="{{ route('contacts.create') }}">Add Contact</a></li>
+        <li><a href="{{ route('contacts.show', 1) }}">Show a Contact</a></li>
+    </div>
+</html>
